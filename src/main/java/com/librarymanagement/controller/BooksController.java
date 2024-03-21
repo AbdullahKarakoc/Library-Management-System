702e868bd0261502eb4632bbec893001799f993a
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-03-21 16:25:49 +0300

    feat:

--- src/main/java/com/librarymanagement/controller/BooksController.java
+++ src/main/java/com/librarymanagement/controller/BooksController.java
@@ -1,5 +1,6 @@
 package com.librarymanagement.controller;
 
+import com.librarymanagement.component.SwaggerAnnotations;
 import com.librarymanagement.domain.request.BooksRequestDto;
 import com.librarymanagement.domain.response.BooksResponseDto;
 import com.librarymanagement.service.BooksService;
@@ -13,27 +14,21 @@ import java.util.List;
 
 @RequestMapping("/api")
 @RestController
-@Tag(name = "Books-Controller")
+@Tag(name = "Books-Controller", description = "Controller managing operations related to books")
 public class BooksController {
     @Autowired
     private BooksService service;
-
+    @Autowired
+    private SwaggerAnnotations swaggerAnnotations;
 
     @Operation(
-            description = "Post endpoint for admin",
-            summary = "This is a summary for management post endpoint",
+            summary = "Adds a new book",
+            description = "An endpoint used by administrators to add a new book.",
             responses = {
-                    @ApiResponse(
-                            description = "Success",
-                            responseCode = "200"
-                    ),
-                    @ApiResponse(
-                            description = "Unauthorized",
-                            responseCode = "403"
-                    )
+                    @ApiResponse(responseCode = "200", description = "Successfully added"),
+                    @ApiResponse(responseCode = "403", description = "Unauthorized access")
             }
     )
-
     @PostMapping("/books")
     public BooksRequestDto addBooks(@RequestBody BooksRequestDto booksDto){
         return service.saveBook(booksDto);
@@ -41,46 +36,72 @@ public class BooksController {
 
 
     @Operation(
-            description = "Get endpoint for all users",
-            summary = "This is a summary for management get endpoint",
+            summary = "Lists all books",
+            description = "An endpoint used to list all books.",
             responses = {
-                    @ApiResponse(
-                            description = "Success",
-                            responseCode = "200"
-                    ),
-                    @ApiResponse(
-                            description = "Unauthorized",
-                            responseCode = "403"
-                    )
+                    @ApiResponse(responseCode = "200", description = "Successful"),
+                    @ApiResponse(responseCode = "403", description = "Unauthorized access")
             }
     )
-
     @GetMapping("/books")
     public List<BooksResponseDto> findAllBooks(){
         return service.getBooks();
     }
+
+    @Operation(
+            summary = "Searches for a book by ID",
+            description = "Searches for a specific book by its ID.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful"),
+                    @ApiResponse(responseCode = "404", description = "Book not found")
+            }
+    )
     @GetMapping("/booksById/{id}")
     public BooksResponseDto findBookById(@PathVariable int id){
         return service.getBookById(id);
     }
+
+
+    @Operation(
+            summary = "Searches for a book by name",
+            description = "Searches for a specific book by its name.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful"),
+                    @ApiResponse(responseCode = "404", description = "Book not found")
+            }
+    )
     @GetMapping("/booksByName/{name}")
     public BooksResponseDto findBookByName(@PathVariable String name){
         return service.getBookByName(name);
     }
 
 
+    @Operation(
+            summary = "Updates a book",
+            description = "Updates the information of an existing book.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful"),
+                    @ApiResponse(responseCode = "404", description = "Book not found")
+            }
+    )
     @PutMapping("/books/{id}")
     public BooksResponseDto updateBook(@PathVariable int id, @RequestBody BooksRequestDto booksDto){
         return service.updateBook(id, booksDto);
     }
 
 
+    @Operation(
+            summary = "Deletes a book",
+            description = "Deletes an existing book.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful"),
+                    @ApiResponse(responseCode = "404", description = "Book not found")
+            }
+    )
     @DeleteMapping("/books/{id}")
     public String deleteBook(@PathVariable int id){
         return service.deleteBook(id);
     }
-
-
 
 
 
--- src/main/java/com/librarymanagement/controller/UsersController.java
+++ src/main/java/com/librarymanagement/controller/UsersController.java
@@ -1,8 +1,11 @@
 package com.librarymanagement.controller;
 
+import com.librarymanagement.component.SwaggerAnnotations;
 import com.librarymanagement.domain.model.OurUser;
 import com.librarymanagement.repository.OurUserRepo;
 import com.librarymanagement.repository.BooksRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@Tag(name = "Users-Controller")
+@Tag(name = "Users-Controller", description = "Controller managing operations related to users")
 public class UsersController {
     @Autowired
     private OurUserRepo ourUserRepo;
@@ -22,12 +25,28 @@ public class UsersController {
     private BooksRepository booksRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private SwaggerAnnotations swaggerAnnotations;
 
+
+    @Operation(
+            summary = "Go to the homepage",
+            description = "An endpoint to go to the general homepage without requiring authentication."
+    )
     @GetMapping("/")
     public String goHome() {
         return "This is publickly accesible withing needing authentication ";
     }
 
+
+    @Operation(
+            summary = "Save a new user",
+            description = "An endpoint used to save a new user.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User successfully saved"),
+                    @ApiResponse(responseCode = "404", description = "Error, user not saved")
+            }
+    )
     @PostMapping("/users")
     public ResponseEntity<Object> saveUSer(@RequestBody OurUser ourUser) {
         ourUser.setPassword(passwordEncoder.encode(ourUser.getPassword()));
@@ -37,12 +56,22 @@ public class UsersController {
         }
         return ResponseEntity.status(404).body("Error, User Not Saved");
     }
-    
+
+
+    @Operation(
+            summary = "Get all users",
+            description = "An endpoint used to list all users."
+    )
     @GetMapping("/users")
     public ResponseEntity<Object> getAllUsers() {
         return ResponseEntity.ok(ourUserRepo.findAll());
     }
 
+
+    @Operation(
+            summary = "Get my details",
+            description = "An endpoint used to get details of the logged-in user."
+    )
     @GetMapping("/users/single")
     public ResponseEntity<Object> getMyDetails() {
         return ResponseEntity.ok(ourUserRepo.findByEmail(getLoggedInUserDetails().getUsername()));
